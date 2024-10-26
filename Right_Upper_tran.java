e7111b74c328504fc31b58d56ebff6b26f2af28c
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2024-10-26 20:12:33 +0530

    Add file

--- Right_Upper_tran.java
+++ Right_Upper_tran.java
@@ -1,10 +1,10 @@
-/* .      *
+/*       *
          **
         ***
        ****
       *****
 */
-    public class Main
+    class Main
 {
 	public static void main(String[] args) {
 	    int n= 5;
